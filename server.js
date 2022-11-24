b8aa42e6117870dfa06c8660f6e9ce129f151f6d
AMAL <mailtoamaldevcd@gmail.com>
2022-11-25 02:49:00 +0530

    connection pending

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var mysql = require('mysql');
+var app=express();
+
+var msg="";
+
+var connection = mysql.createConnection({
+    
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'',
+});
+
+connection.connect(function(error)
+{
+    if(!!error)
+    {
+        console.log('Error');
+        msg="Error";
+    }
+    else
+    {
+        console.log('Connected');
+        msg="Connected";
+    }
+})
+
+app.get('/',(req,res) => {
+    
+    connection.query("Select * from sample",function(error,rows,fields){
+        if(!!error)
+        {
+            console.log('Error in Query');
+        }
+        else
+        {
+            console.log('Successful Query');
+        }
+    })
+})
+
+app.listen(3000);
